fc16d237311f74acbaf2804c2277c2dcd06cd729
smitipachnanda <pachnandasmiti@gmail.com>
2021-02-27 20:50:35 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+import * as firebase from firebase
+require('@firebase/firestore')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDzMbAIoBqh9JZGLRdpGBK7ae6y8s-x_l0",
+    authDomain: "willy-d4f37.firebaseapp.com",
+    projectId: "willy-d4f37",
+    storageBucket: "willy-d4f37.appspot.com",
+    messagingSenderId: "457744026304",
+    appId: "1:457744026304:web:b9c1912bf8d173bebe3b25",
+    measurementID:"willy-d4f37"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
